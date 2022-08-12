56b50cda9c4335ee3a55abdd65b3b144b721cfa7
frkntkny <80395899+frkntkny@users.noreply.github.com>
2022-08-12 14:51:15 +0100

    spatie package added and admin role has setted it up

--- maybereddit/database/seeders/DatabaseSeeder.php
+++ maybereddit/database/seeders/DatabaseSeeder.php
@@ -14,12 +14,14 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-//    public function run()
-//    {
-//       // User::factory(100)->has(Post::factory())->create();
+    public function run()
+    {
+       // User::factory(100)->has(Post::factory())->create();
 //        User::factory(100)->create()->each(function($user){
 //        $posts = Post::factory(rand(1,5))->make();
 //        $user->posts()->saveMany($posts);
 //    });
-//    }
+        $this->call(RoleSeeder::class);
+        $this->call(AdminSeeder::class);
+    }
 }
