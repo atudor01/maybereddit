9e49bb4058feb924e97c0eab666c2a6bfe670926
frkntkny <80395899+frkntkny@users.noreply.github.com>
2022-08-16 17:04:17 +0100

    404 and 403 error pages added
    404 and 403 error pages added

---
+++ maybereddit/resources/views/errors/403.blade.php
@@ -0,0 +1,7 @@
+<x-header></x-header>
+<div class="container mx-auto min-h-full " style="background-color: #00ADB5">
+    <div class="grid h-screen place-items-center">
+        <img class="" src="{{url('/assets/403.png')}}">
+    </div>
+</div>
+<x-footer></x-footer>
