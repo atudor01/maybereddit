4d525d0578e7df8a8c5c6e9f949a3d097765bebb
Alxandru Tudora <lex.udora@gmail.com>
2022-09-02 12:06:04 +0100

    dfs

--- maybereddit/resources/views/errors/403.blade.php
+++ maybereddit/resources/views/errors/403.blade.php
@@ -1,7 +1,11 @@
-<x-header></x-header>
-<div class="container mx-auto min-h-full " style="background-color: #00ADB5">
-    <div class="grid h-screen place-items-center">
-        <img class="" src="{{url('/assets/403.png')}}">
-    </div>
-</div>
-<x-footer></x-footer>
+<x-guest-layout>
+
+    <x-slot name="slot">
+        <div class="container mx-auto min-h-full " style="background-color: #00ADB5">
+            <div class="grid h-screen place-items-center">
+                <img class="" src="{{url('/assets/403.png')}}">
+            </div>
+        </div>
+    </x-slot>
+
+</x-guest-layout>
